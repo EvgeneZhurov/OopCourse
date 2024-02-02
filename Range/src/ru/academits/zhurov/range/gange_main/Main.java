b3a50098a9607e9cc852b072ac47f9d03a97e882
EvgeneZhurov <zhurovev@yandex.ru>
2024-02-02 13:24:39 +0300

    Добавлен проект Range

---
+++ Range/src/ru/academits/zhurov/range/gange_main/Main.java
@@ -0,0 +1,7 @@
+package ru.academits.zhurov.range.gange_main;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Range/src/ru/academits/zhurov/range/range/Range.java
@@ -0,0 +1,5 @@
+package ru.academits.zhurov.range.range;
+
+public class Range {
+
+}
