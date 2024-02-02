0a0af0b985d241f1a51883eb7ce7cd6c492a9cbc
EvgeneZhurov <zhurovev@yandex.ru>
2024-02-02 14:36:00 +0300

    1 Часть задачи Range готова к проверке.

--- Range/src/ru/academits/zhurov/range/gange_main/Main.java
+++ Range/src/ru/academits/zhurov/range/gange_main/Main.java
@@ -1,7 +1,27 @@
 package ru.academits.zhurov.range.gange_main;
 
+import ru.academits.zhurov.range.range.Range;
+
 public class Main {
     public static void main(String[] args) {
+        Range range = new Range(1, 10);
 
+        System.out.println("Начало диапазона: " + range.getFrom());
+        System.out.println("Конец диапазона: " + range.getTo());
+
+        double coordinate = 13.0;
+
+        if (range.isInside(coordinate)) {
+            System.out.println("Координата " + coordinate + " принадлежит диапазону от " + range.getFrom() + " до " + range.getTo());
+        } else {
+            System.out.println("Координата " + coordinate + " не принадлежит диапазону от " + range.getFrom() + " до " + range.getTo());
+        }
+
+        System.out.println("Длина диапазона = " + range.getRangeLength());
+
+        range.setFrom(5);
+        range.setTo(20);
+
+        System.out.println("Длина диапазона после изменения = " + range.getRangeLength());
     }
 }
--- Range/src/ru/academits/zhurov/range/range/Range.java
+++ Range/src/ru/academits/zhurov/range/range/Range.java
@@ -1,5 +1,35 @@
 package ru.academits.zhurov.range.range;
 
 public class Range {
+    private double from;
+    private double to;
 
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getRangeLength() {
+        return this.to - this.getFrom();
+    }
+
+    public boolean isInside(double value) {
+        return this.from <= value && this.to >= value;
+    }
 }
