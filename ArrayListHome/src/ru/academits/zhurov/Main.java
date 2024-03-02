37d65c481fe389bd7c296a3f8d81edb651799f5f
EvgeneZhurov <zhurovev@yandex.ru>
2024-03-02 11:25:56 +0300

    1 вариант задачи для отправки.

---
+++ ArrayListHome/src/ru/academits/zhurov/Main.java
@@ -0,0 +1,55 @@
+package ru.academits.zhurov;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> fileReader = new ArrayList<>();
+
+        String absolutPath = "C:\\Users\\admin\\IdeaProjects\\OopCourse\\input.html";
+        try (Scanner scanner = new Scanner(new File(absolutPath))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                fileReader.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Чтение из файла: " + fileReader);
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            list.add(i);
+        }
+
+        System.out.println("Заполненный список: " + list);
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) % 2 == 0) {
+                list.remove(i);
+                i--;
+            }
+        }
+
+        System.out.println("Список без четных чисел: " + list);
+
+        list.clear();
+        list = Arrays.asList(1, 5, 1, 2, 3, 5);
+        List<Integer> listWithoutDuplicates = new ArrayList<>();
+
+        for (Integer integer : list) {
+            if (!listWithoutDuplicates.contains(integer)) {
+                listWithoutDuplicates.add(integer);
+            }
+        }
+
+        System.out.println("Список без дублей: " + listWithoutDuplicates);
+    }
+}
