75309736782eacc4e04df9df96ae0c2a88899548
EvgeneZhurov <zhurovev@yandex.ru>
2024-03-02 10:17:30 +0300

    Внесены изменения согласно замечаниям

---
+++ Shapes/src/ru/academits/zhurov/comparator/AreaComparator.java
@@ -0,0 +1,13 @@
+package ru.academits.zhurov.comparator;
+
+import ru.academits.zhurov.shapes.Shape;
+
+import java.util.Comparator;
+
+public class AreaComparator implements Comparator<Shape> {
+
+    @Override
+    public int compare(Shape o1, Shape o2) {
+        return Double.compare(o1.getArea(), o2.getArea());
+    }
+}
