9868c60d72a7e88b1c876bf64fe8be6112705ece
EvgeneZhurov <zhurovev@yandex.ru>
2024-03-09 15:58:08 +0300

    Внесены изменения согласно замечаниям

--- ArrayListHome/src/ru/academits/zhurov/arrayListHome/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/zhurov/arrayListHome/ArrayListHome.java
@@ -1,5 +1,46 @@
 package ru.academits.zhurov.arrayListHome;
 
-public class ArrayListHome {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class ArrayListHome {
+    public static List<String> splitFileStringsList(String pathName) {
+        List<String> fileStringsList = new ArrayList<>();
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(pathName));
+            String fileString;
+
+            while ((fileString = bufferedReader.readLine()) != null) {
+                fileStringsList.add(fileString);
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла");
+        }
+
+        return fileStringsList;
+    }
+
+    public static List<Integer> getListWithoutEvenNumbers(List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) % 2 == 0) {
+                list.remove(i);
+                i--;
+            }
+        }
+
+        return list;
+    }
+
+    public static List<Integer> getListWithoutDuplicates(List<Integer> list) {
+        List<Integer> listWithoutDuplicates = new ArrayList<>();
+
+        for (Integer integer : list) {
+            if (!listWithoutDuplicates.contains(integer)) {
+                listWithoutDuplicates.add(integer);
+            }
+        }
+        return listWithoutDuplicates;
+    }
 }
--- ArrayListHome/src/ru/academits/zhurov/arrayListHome_main/Main.java
+++ ArrayListHome/src/ru/academits/zhurov/arrayListHome_main/Main.java
@@ -1,79 +1,35 @@
 package ru.academits.zhurov.arrayListHome_main;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
+
+import static ru.academits.zhurov.arrayListHome.ArrayListHome.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> fileReader = new ArrayList<>();
+        String pathName = "ArrayListHome/src/ru/academits/zhurov/arrayListHome/input.html";
+        List<String> fileStringsList = splitFileStringsList(pathName);
 
-        try (Scanner scanner = new Scanner(new File("ArrayListHome/src/ru/academits/zhurov/arrayListHome/input.html"))) {
-            while (scanner.hasNextLine()) {
-                fileReader.add(scanner.nextLine());
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Список строк, прочитанный из файла: " + fileStringsList);
 
-        System.out.println("Чтение из файла: " + fileReader);
-
-        List<Integer> list = new ArrayList<>();
+        List<Integer> randomNumbersList = new ArrayList<>();
 
         for (int i = 0; i < 100; i++) {
-            list.add(i);
+            randomNumbersList.add((int) (Math.random() * 100) + 1);
         }
 
-        System.out.println("Заполненный список: " + list);
+        System.out.println("Заполненный список: " + randomNumbersList);
+        getListWithoutEvenNumbers(randomNumbersList);
+        System.out.println("Список без четных чисел: " + randomNumbersList);
 
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) % 2 == 0) {
-                list.remove(i);
-                i--;
-            }
-        }
+        List<Integer> integerList = new ArrayList<>(5);
+        integerList.add(1);
+        integerList.add(5);
+        integerList.add(2);
+        integerList.add(3);
+        integerList.add(5);
 
-        System.out.println("Список без четных чисел: " + list);
-
-        list.clear();
-        list = Arrays.asList(1, 5, 1, 2, 3, 5);
-        List<Integer> listWithoutDuplicates = new ArrayList<>();
-
-        for (Integer integer : list) {
-            if (!listWithoutDuplicates.contains(integer)) {
-                listWithoutDuplicates.add(integer);
-            }
-        }
-
+        List<Integer> listWithoutDuplicates = getListWithoutDuplicates(integerList);
         System.out.println("Список без дублей: " + listWithoutDuplicates);
     }
 }
-
-        /*
-        1. http://joxi.ru/29yZ7P8sjB5gz2 - сейчас модуль ArrayListHome в репозитории не настроен как модуль.
-        Возможно, в репозиторий не внесены изменения в файлах из папки .idea
-        В текущем варианте файл modules.xml не содержит настройки ArrayListHome.
-        Нужно запушить текущие изменения в репозиторий, либо, если не получится, вручную поменять modules.xml и запушить изменения
-
-        2. Пакет ru.academits.zhurov - сейчас имя пакета не содержит название задачи
-
-        3. Лучше каждую подзадачу оформить в виде отдельной функции
-
-        4. list - нужно дать более информативное имя, чтобы было понятно что именно хранит переменная
-
-        5. fileReader - имя неправильное, т.к. это не Reader, а список строк файла
-
-        6. Плохо печатать пользователю стек вызовов в случае ошибки.
-        Лучше распечатать информативное сообщение
-
-        7. "Чтение из файла: " - неинформативное сообщение
-
-        8. Для последней подзадачи лучше сделать отдельную переменную для списка
-
-        9. В последней подзадаче новому списку лучше задать подходящую начальную вместимость
-
-        10. Для чтения строк из файла для эффективности лучше использовать BufferedReader от FileReader вместо Scanner
-        */
