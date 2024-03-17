b8d80015c1115d9aa84db9e42826a4ad9cb92721
EvgeneZhurov <zhurovev@yandex.ru>
2024-03-17 16:30:33 +0300

    Попытка внесения изменений в файл modules.xml, исправлены замечания к задаче ArrayListHome

---
+++ ArrayListHome/src/ru/academits/zhurov/array_list_home/Main.java
@@ -0,0 +1,83 @@
+package ru.academits.zhurov.array_list_home;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> fileLinesList = null;
+
+        try {
+            fileLinesList = getFileLines("ArrayListHome/src/ru/academits/zhurov/array_list_home/input.html");
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла");
+        }
+
+        if (fileLinesList != null) {
+            System.out.println("Список строк, прочитанный из файла: " + fileLinesList);
+        }
+
+        List<Integer> randomNumbers = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            randomNumbers.add(new Random().nextInt(101));
+        }
+
+        System.out.println("Заполненный список: " + randomNumbers);
+        removeEvenNumbers(randomNumbers);
+        System.out.println("Список без четных чисел: " + randomNumbers);
+
+        List<Integer> integers = new ArrayList<>(5);
+        integers.add(1);
+        integers.add(5);
+        integers.add(2);
+        integers.add(3);
+        integers.add(5);
+
+        List<Integer> withoutDuplicatesIntegers = getListWithoutDuplicates(integers);
+        System.out.println("Список без дублей: " + withoutDuplicatesIntegers);
+    }
+
+    public static List<String> getFileLines(String fileName) throws IOException {
+        List<String> fileLinesList = new ArrayList<>();
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        String fileLine;
+
+        while ((fileLine = bufferedReader.readLine()) != null) {
+            fileLinesList.add(fileLine);
+        }
+
+        bufferedReader.close();
+
+        return fileLinesList;
+    }
+
+    public static void removeEvenNumbers(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) % 2 == 0) {
+                numbers.remove(i);
+                i--;
+            }
+        }
+    }
+
+    public static <E> List<E> getListWithoutDuplicates(List<E> list) {
+        List<E> withoutDuplicates = new ArrayList<>(list.size());
+
+        for (E item : list) {
+            if (!withoutDuplicates.contains(item)) {
+                withoutDuplicates.add(item);
+            }
+        }
+
+        return withoutDuplicates;
+    }
+}
